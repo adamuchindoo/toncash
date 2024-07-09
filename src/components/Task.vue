62a627a55da692ec0992470487056cd0ed3e272d
adamuchindoo <49189545+adamuchindoo@users.noreply.github.com>
2024-07-09 04:47:14 +0100

    get info

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,15 +1,43 @@
 <template>
     <div>
-
+      <h1>Welcome to Our Website</h1>
+      <button @click="getUserId">Get Telegram User ID</button>
     </div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TelegramAuth',
+    methods: {
+      getUserId() {
+        const user = window.Telegram.WebApp.initDataUnsafe.user;
+        if (user) {
+          alert('Your Telegram User ID is: ' + user.id);
+          this.sendUserIdToServer(user.id);
+        } else {
+          alert('No user information available.');
+        }
+      },
+      sendUserIdToServer(userId) {
+        fetch('/save-user-id', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ userId })
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      }
+    },
+    mounted() {
+      window.Telegram.WebApp.ready();
+    }
+  }
+  </script>
+  
